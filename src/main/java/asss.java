d482a6e29c2aaba7bcd489d5d7da5a2e3b1c7d2b
mchikunov <udafov@yandex.ru>
2019-01-01 21:31:57 +0300

    q

---
+++ src/main/java/asss.java
@@ -0,0 +1,10 @@
+public class asss {
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+             static void sayHello(){
+                System.out.println("sdf");
+        }
+    }
+
